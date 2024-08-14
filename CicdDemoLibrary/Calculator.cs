55d8ec8f35770978366d3fa69d1a1f0952e0838e
Grand Silence <mixtape774@gmail.com>
2024-08-14 18:13:07 +0300

    :boom: Broken calculator sum method.

--- CicdDemoLibrary/Calculator.cs
+++ CicdDemoLibrary/Calculator.cs
@@ -2,7 +2,7 @@
 
 public sealed class Calculator : ICalculator
 {
-    public int Sum(int a, int b) => a + b;
+    public int Sum(int a, int b) => a + b + a + b; // TODO: fix it
 
     public int Sub(int a, int b) => a - b;
 
