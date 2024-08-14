e3216de396fdd02ac11453f768e41a09b6d2353e
Grand Silence <mixtape774@gmail.com>
2024-08-14 18:44:46 +0300

    :rocket: update github ci/cd: publish to nuget org

--- CicdDemoLibrary/Calculator.cs
+++ CicdDemoLibrary/Calculator.cs
@@ -2,7 +2,7 @@
 
 public sealed class Calculator : ICalculator
 {
-    public int Sum(int a, int b) => a + b + a + b; // TODO: fix it
+    public int Sum(int a, int b) => a + b;
 
     public int Sub(int a, int b) => a - b;
 
